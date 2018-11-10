784c54545eded344449725919129be0907454056
keeperlee <leejamesjian@163.com>
2018-11-10 18:45:09 +0800

    删除二分搜索树的任意元素

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,26 @@ public class Main {
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<>();
-        Random random = new Random();
-        int n = 1000;
-        //test removeMin
-        for (int i=0;i<n;i++)
-            bst.add(random.nextInt(10000));
-
-        ArrayList<Integer> nums = new ArrayList<>();
-        while (!bst.isEmpty())
-            nums.add(bst.removeMin());
-
-        System.out.println(nums);
-        for (int i=1;i<nums.size();i++)
-            if(nums.get(i-1)>nums.get(i))
-                throw new IllegalArgumentException("Error");
-        System.out.println("removeMin test completed");
+        int[] nums = {5,3,6,8,4,2};
+        for (int num:nums) {
+            bst.add(num);
+            ///////////////////////
+            //          5       //
+            //      /       \   //
+            //     3        6   //
+            //   /  \         \ //
+            //  2    4        8 //
+            //////////////////////
+        }
+        bst.preOrder();
+        System.out.println();
 
 
-        //test removeMin
-        for (int i=0;i<n;i++)
-            bst.add(random.nextInt(10000));
-        nums = new ArrayList<>();
-        while (!bst.isEmpty())
-            nums.add(bst.removeMax());
+        System.out.println("删除后：");
+        bst.remove(6);
+        bst.preOrder();
+        System.out.println();
 
-        System.out.println(nums);
-        for(int i = 1 ; i < nums.size() ; i ++)
-            if(nums.get(i - 1) < nums.get(i))
-                throw new IllegalArgumentException("Error!");
-        System.out.println("removeMax test completed.");
+
     }
 }
